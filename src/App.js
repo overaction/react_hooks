5e04eeb46712348e54a3b948681a32d9521103bc
overaction <52850392+overaction@users.noreply.github.com>
2020-10-20 23:53:11 +0900

    0. class component recap
    componentDidMount
    componentDidUpdate
    componentWillunmount
    render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState} from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log('constructor');
+    }
+    state = {
+      count: 0
+    };
+    add = () => {
+      this.setState(current => ({count: current.count + 1}))
+    };
+    remove = () => {
+      this.setState(current => ({count : current.count -1}));
+    };
+    componentDidMount() {
+      console.log('component rendered');
+    }
+    componentDidUpdate() {
+      console.log('i just updated');
+    }
+    componentWillUnmount() {
+      console.log('goodbye');
+    }
+    render() {
+      console.log('hello.. rendering');
+      return (
+        <div>
+          <h1>hello.. {this.state.count}</h1>
+          <button onClick={this.add}>add</button>
+          <button onClick={this.remove}>minus</button>
+        </div>
+      )
+    }
+} 
 
 export default App;
