68b13ab3cf1011efdbe548dee231c928d1af61b1
overaction <52850392+overaction@users.noreply.github.com>
2020-10-21 15:35:08 +0900

    Update useTabs.js

--- src/useTabs.js
+++ src/useTabs.js
@@ -1,5 +1,8 @@
 export const useTabs = (array, index) => {
     const [currentIndex, setCurrentIndex] = useState(index);
+    if(!array || !Array.isArray(array)) {
+    return;
+    }
     return {
     currentContent: array[currentIndex],
     onchange: setCurrentIndex
